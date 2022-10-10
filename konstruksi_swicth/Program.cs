2c51bbfba1c669a50d62c07e10e90742451f9c3a
Finaaaa17 <109045559+Finaaaa17@users.noreply.github.com>
2022-10-10 22:20:14 +0700

    Add files via upload

---
+++ konstruksi_swicth/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace konstruksi_swicth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeLevel = 100;
+            string employeeName = "John Smith";
+
+            string title = "";
+
+            switch (employeeLevel)
+            {
+                case 100:
+                case 200:
+                    title = "Senior Associate";
+                    break;
+                case 300:
+                    title = "Manager";
+                    break;
+                case 400:
+                    title = "Senior Manager";
+                    break;
+                default:
+                    title = "Associate";
+                    break;
+            }
+
+            Console.WriteLine($"{employeeName}, {title}");
+        }
+    }
+}
